f11f529249f1c818be45d4c887877aac2371fabd
yassin991 <yassenelsherbiny69@gmail.com>
2026-04-05 01:29:04 +0200

    Question01

--- AssignmentLinq01/Program.cs
+++ AssignmentLinq01/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Question01
+
+            1.Get all products from the "Seafood" category.Print each
+              product's name and price.
+
+                List<Product> ProductList = new List<Product>();
+            var seafoodProducts = ProductList
+    .Where(p => p.Category == "Seafood");
+
+            foreach (var p in seafoodProducts)
+            {
+                Console.WriteLine($"{p.ProductName} - {p.UnitPrice}");
+            }
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
